b340652afd6b89afafacc26c33d65f0d6a393f79
Kseniya Lyubasova <esenia@ngs.ru>
2015-09-21 11:59:45 +0300

    new

---
+++ src/app.php
@@ -0,0 +1,123 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Response;
+use Silex\Provider\FormServiceProvider;
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+$app->register(new Silex\Provider\TwigServiceProvider(), array(
+    'twig.path' => __DIR__.'/views',
+));
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'db.options' => array(
+            'driver'    => 'pdo_mysql',
+            'host'      => 'localhost',
+            'dbname'    => 'epic_blog',
+            'user'      => 'root',
+            'password'  => 'root',
+            'charset'   => 'utf8',
+        ),
+    )
+);
+$app->register(new FormServiceProvider());
+$app->register(new Silex\Provider\ValidatorServiceProvider());
+$app->register(new Silex\Provider\TranslationServiceProvider(), array(
+    'translator.domains' => array(),
+));
+
+$app->register(new Silex\Provider\SecurityServiceProvider(), array(
+
+));
+$app['security.firewalls'] = array(
+    'admin' => array(
+        'pattern' => '^/admin',
+        'http' => true,
+        'users' => array(
+            // raw password is foo
+            'admin' => array('ROLE_ADMIN', '5FZ2Z8QIkA7UTZ4BYkoC+GsReLf569mSKDsfods6LYQ8t+a8EW9oaircfMpmaLbPBh4FOBiiFyLfuZmTSUwzZg=='),
+        ),
+    ),
+);
+
+$app->get('/', function() use ($app) {
+    $sql = "SELECT * FROM posts";
+    $posts = $app['db']->fetchAll($sql);
+
+    return $app['twig']->render('index.html.twig', [
+        'posts' => $posts,
+    ]);
+});
+
+$app->get('/post/{id}', function($id) use ($app) {
+    $sql = "
+        SELECT * 
+        FROM posts p
+        WHERE p.id = :id
+    ";
+    $post = $app['db']->fetchAssoc($sql, [
+        ':id' => $id,
+    ]);
+
+    $sql = "
+        SELECT * 
+        FROM comments c
+        WHERE c.post_id = :id
+    ";
+    $comments = $app['db']->fetchAll($sql, [
+        ':id' => $id,
+    ]);
+
+    return $app['twig']->render('post.html.twig', [
+        'post' => $post,
+        'comments' => $comments,
+    ]);
+});
+
+
+$app->match('/admin/add', function() use ($app) {
+    $form = $app['form.factory']->createBuilder('form')
+        ->add('title')
+        ->add('content', 'textarea')
+        ->getForm();
+    
+
+    $form->handleRequest($app['request']);
+
+    if ($form->isValid()) {
+        $data = $form->getData();
+        
+        // do something with the data
+        $sql = "INSERT ...";
+
+        // redirect somewhere
+        return $app->redirect('/');
+    }
+    return $app['twig']->render(
+        'add.html.twig', [
+            'form' => $form->createView()
+        ]);
+});
+
+$app->get('/admin/edit/{id}', function($id) use ($app) {
+    return '';
+});
+
+$app->get('/admin/delete/{id}/', function($id) use ($app) {
+    return '';
+});
+
+$app->get('/add-comment', function() use ($app) {
+    return '';
+});
+
+$app->get('/edit-comment/{id}', function($id) use ($app) {
+    return '';
+});
+
+$app->get('/delete-comment/{id}', function($id) use ($app) {
+    return '';
+});
+
+
+return $app;
